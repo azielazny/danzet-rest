9f355acd3ec7f6910f59c9cc3cbd87e2b6285a75
azielazny <azielazny@gmail.com>
2018-02-08 20:07:35 +0100

    Initial commit plus slim framework activity test

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arek50
+ * Date: 08.02.2018
+ * Time: 19:01
+ */
+$config = [
+    'db' => [
+        'servername' =>'localhost',
+        'username' => 'root',
+        'password' => '',
+        'dbname' => 'auto-danzet',
+    ]
+];
